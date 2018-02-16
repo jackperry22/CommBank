5ede14fb737640b9b18dd509003fe9307dfd7056
Jack Perry <jackperry@192-168-1-157.tpgi.com.au>
2018-02-16 15:34:50 +1100

    Initial Commit

---
+++ Commonwealth BankTests/Commonwealth_BankTests.swift
@@ -0,0 +1,36 @@
+//
+//  Commonwealth_BankTests.swift
+//  Commonwealth BankTests
+//
+//  Created by Jack Perry on 16/2/18.
+//  Copyright © 2018 Jack Perry. All rights reserved.
+//
+
+import XCTest
+@testable import Commonwealth_Bank
+
+class Commonwealth_BankTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
